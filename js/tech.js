050d90db1b58efa4990a12aac0b9ddd30ddb8ebf
Jake Toolson <>
2024-08-24 16:59:07 -0600

    update

---
+++ js/tech.js
@@ -0,0 +1,13 @@
+
+function getIcon(name) {
+  let map = {
+    aws: "aws.png",
+    php: "php.png",
+    laravel: "laravel.png",
+    docker: "docker.png",
+    vite: "vite.png",
+    vue: "vue.png",
+    node: "node.png",
+    nest: "nest.png"
+  }
+}
